c5364590b73d76aab082d3cbbad59b03fc189278
cema-sp <cema.rus@gmail.com>
2015-10-20 10:58:00 -0400

    Implements mario

--- week1/mario/mario.c
+++ week1/mario/mario.c
@@ -1,7 +1,32 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-		printf("hello\n");
+    int maxHeight = 23;
+    int topWidth = 2;
+    int height;
+    int width;
+
+    do
+    {
+        printf("height: ");
+        height = GetInt();
+    } while (height < 0 || height > maxHeight);
+    
+    width = height + topWidth - 1;
+
+    for (int i = topWidth; i <= width; i++)
+    {
+        for (int j = 0; j < width - i; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k < i; k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
 
